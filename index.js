7140c8775ddbc50c3921be5386e545876410c037
Brian <mail@standinshd.site>
2020-07-13 08:41:39 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+const http = require('http');
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "hbs");
+app.use("/assets", express.static("assets"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 250000);
+
+app.get("/", (req, res) => {
+  console.log(Date.now() + " Ping Received");
+  res.status(200).render("index");
+});
+
+app.listen(3000, "192.168.0.105", console.log("Listening on port 3000!"));
