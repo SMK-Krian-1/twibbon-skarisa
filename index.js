add918a803c5eaa55b3513db39ec936f3cb78b89
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-09-07 02:41:54 +0700

    fix(modules): delete hbs variable due to unused

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const hbs = require("hbs");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const http = require('http');
 
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "hbs");
-app.use("/assets", express.static("assets"));
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'hbs');
+app.use('/assets', express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  http.get(`http://twibbon-um.reng.my.id/`);
 }, 250000);
 
-app.get("/", (req, res) => {
-  console.log(Date.now() + " Ping Received");
-  res.status(200).render("index");
+app.get('/', (req, res) => {
+  console.log(Date.now() + ' Ping Received');
+  res.status(200).render('index');
 });
 
-app.listen(3000, console.log("Listening on port 3000!"));
+app.listen(3000, console.log('Listening on port 3000!'));
