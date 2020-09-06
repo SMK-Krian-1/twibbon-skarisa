5ee3f04f20824ef3702c39302e734f7385f768c1
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-09-07 04:04:27 +0700

    feat!(handlebars): Use hbs for twibbon selection

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
 const http = require('http');
+const { json } = require('body-parser');
+const twibbonData = 
 
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'src'));
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
 app.set('view engine', 'hbs');
 app.use('/assets', express.static('assets'));
 app.use(bodyParser.json());
@@ -16,7 +23,18 @@ setInterval(() => {
 
 app.get('/', (req, res) => {
   console.log(Date.now() + ' Ping Received');
-  res.status(200).render('index');
+  res.status(200).render('index', {
+    twibbon: [
+      {
+        name: 'Test Twibbon',
+        image: 'testtwibbon'
+      },
+      {
+        name: 'Ordinary twibbon',
+        image: 'ordinarytwibbon'
+      }
+    ]
+  });
 });
 
 app.listen(3000, console.log('Listening on port 3000!'));
